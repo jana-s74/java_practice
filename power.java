94b6b6027f768eab8d6a477ddbf33476a6d3089d
jana-s74 <janaselvarasu7@gmail.com>
2026-06-22 10:09:07 +0530

    new program

---
+++ power.java
@@ -0,0 +1,7 @@
+public class power {
+    public static void main(String[] args) {
+        int number=5;
+        int power=number*2;
+        System.out.println("The power of "+number+" is: "+power);
+    }
+}
