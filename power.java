5119235d65cba0efae98040b07f5bef5e7897414
jana-s74 <janaselvarasu7@gmail.com>
2026-06-24 11:56:42 +0530

    Add various string manipulation and utility programs in Java

--- power.java
+++
@@ -1,7 +0,0 @@
-public class power {
-    public static void main(String[] args) {
-        int number=5;
-        int power=number*2;
-        System.out.println("The power of "+number+" is: "+power);
-    }
-}
